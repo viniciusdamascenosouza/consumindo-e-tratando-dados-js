b7cf81d2217b63166110b80e9e6790fcd01bd3c1
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-04 09:06:19 -0300

    aula 3 finalizada

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
-.then(r => console.log(r));
+.then(r => {
+    if(r.erro){
+        throw Error('Esse CEP não existe!')
+    } else
+        console.log(r)
+        })
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluído!'));
+
 
 console.log(consultaCEP);
