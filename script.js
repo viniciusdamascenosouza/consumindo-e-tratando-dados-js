1baa8c2a58cd904762b22fc3ebad2747213570c7
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-04 08:03:39 -0300

    aula 1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+console.log("Mandando Oi para um amigo");
+
+function mandaMensagem(){
+    console.log("Tudo bem?")
+    console.log("Irei lhe enviar uma solicitação!")
+    console.log("Solicitação recebida!")
+}
+
+mandaMensagem();
+
+console.log("Tchau!")
