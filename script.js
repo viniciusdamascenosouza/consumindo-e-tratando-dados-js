a3b489efb4550d2d33b3360b51871bd8a6b2561a
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-05 08:05:22 -0300

    aula4 /1

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try{
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPconvertida = await consultaCEP.json();
         if(consultaCEPconvertida.erro){
-            throw('CEP não existente!');
+            throw Error('CEP não existente!');
         }
         console.log(consultaCEPconvertida);
-    } catch(erro){
+        return consultaCEPconvertida;
+    } catch(erro) {
         console.log(erro);
     }
 }
- buscaEndereco();
+
+let ceps = ['01001000','01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+
+
+
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
