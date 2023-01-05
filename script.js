3242d6cbbb8c0954803e087032d3d709eb780293
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-05 08:46:41 -0300

    finalizando curso de consumo e tratamento de dados com JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try{
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPconvertida = await consultaCEP.json();
@@ -16,6 +18,7 @@ async function buscaEndereco(cep){
         console.log(consultaCEPconvertida);
         return consultaCEPconvertida;
     } catch(erro) {
+        mensagemErro.innerHTML = `<p> CEP inválido! Tente novamente</p>`
         console.log(erro);
     }
 }
